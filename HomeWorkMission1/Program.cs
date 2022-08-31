ee01192ad748e96924a9467d7770e49be82dde34
SDozH <soldatov_alex@bk.ru>
2022-08-31 23:38:26 +0300

    Добавьте файлы проекта.

---
+++ HomeWorkMission1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+namespace Unit13Final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //запускаем счетчик
+            var watch = Stopwatch.StartNew();
+            
+            List<string> list = new List<string>();
+            list = File.ReadLines("C:/Users/Dzh/Desktop/Text1.txt").ToList();
+            Console.WriteLine($"Вставка в  List<T> заняла: {watch.Elapsed.TotalMilliseconds}  мс");
+
+
+            LinkedList<string> listLinked = new LinkedList<string>();
+            listLinked.AddFirst(File.ReadAllText("C:/Users/Dzh/Desktop/Text1.txt"));
+            Console.WriteLine($"Вставка в  LinkedList<T> заняла: {watch.Elapsed.TotalMilliseconds}  мс");
+
+        }
+    }
+}
